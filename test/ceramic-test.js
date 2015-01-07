c25e4746826ea6f5bbabf8a7d46b1ae2dd4fad83
Jeswin Kumar <jeswinpk@agilehead.com>
2015-01-07 19:58:01 +0530

    constructModel is not constructEntity. Some test stubs.

--- test/ceramic-test.js
+++ test/ceramic-test.js
@@ -1,20 +1,113 @@
-describe("New customer", function() {
-  var business;
-  var customer;
+var co = require('co');
 
-  before(function*() {
-    yield setup();
-    business = yield Account.create("FooBar Inc");
-    customer = yield business.addCustomer("Mr. Baz");
-  });
+describe("Ceramic Core", function() {
+    var Author, BlogPost;
+    var authorSchema, postSchema;
+    var Ceramic;
 
-  it("should be the only customer", function*() {
-    var count = yield Customer.count({ businessID: business.id });
-    assert.equal(count, 1);
-  });
+    before(function() {
+        return co(function*() {
+            Ceramic = require("../lib/ceramic");
 
-  after(function*() {
-    yield teardown();
-  });
+            var Author = function(params) {
+                for(var key of params) {
+                    this[key] = params[key];
+                }
+            };
 
+            authorSchema = {
+                name: 'author',
+                schema: {
+                    type: 'object',
+                    properties: {
+                        name: { type: 'string' },
+                        location: { type: 'string' },
+                        age: { type: 'number' }
+                    },
+                    required: ['name', 'location']
+                }
+            };
+
+            authorSchema.ctor = Author;
+
+            BlogPost = function(params) {
+                for(var key of params) {
+                    this[key] = params[key];
+                }
+            };
+
+            postSchema = {
+                name: 'post',
+                schema: {
+                    type: 'object',
+                    properties: {
+                        title: { type: 'string' },
+                        content: { type: 'string' },
+                        published: { type: 'string' },
+                        author: { $ref: 'author' }
+                    },
+                    required: ['title', 'content', 'author']
+                }
+            };
+
+            postSchema.ctor = BlogPost;
+
+        });
+    });
+
+
+    it("completeEntitySchema must complete the entitySchema", function() {
+        return co(function*() {
+            var ceramic = new Ceramic();
+            var typeCache = yield* ceramic.init([authorSchema, postSchema]);
+            //TODO: asserts
+        });
+    });
+
+
+    it("completeVirtualEntitySchema must complete the virtualEntitySchema", function() {
+        return co(function*() {
+            var ceramic = new Ceramic();
+            var typeCache = yield* ceramic.init([authorSchema, postSchema]);
+            //TODO: asserts
+        });
+    });
+
+
+    it("init must create a type cache", function() {
+        return co(function*() {
+            var ceramic = new Ceramic();
+            var typeCache = yield* ceramic.init([authorSchema, postSchema]);
+            //TODO: asserts
+        });
+    });
+
+
+    it("constructModel must create a construct a model", function() {
+        return co(function*() {
+            var ceramic = new Ceramic();
+            var typeCache = yield* ceramic.init([authorSchema, postSchema]);
+            //TODO: asserts
+        });
+    });
+
+
+
+    it("updateModel must create a type cache", function() {
+        return co(function*() {
+            var ceramic = new Ceramic();
+            var typeCache = yield* ceramic.init([authorSchema, postSchema]);
+            //TODO: asserts
+        });
+    });
+
+
+
+
+
+    after(function() {
+        return co(function*() {
+            var a = 1;
+        });
+    });
 });
